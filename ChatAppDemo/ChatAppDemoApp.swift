69003d5b4b944249929bebaef8741abadc9a8142
AlinaSosnovich <alina.sosnovich@mail.ru>
2023-10-03 00:38:21 +0300

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Sergei  Sosnovich on 3.10.23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
